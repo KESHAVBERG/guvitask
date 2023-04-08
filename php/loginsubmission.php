76525b67bb964bdfa77560d8818883c884fab271
KESHAVBERG <kingkeshav7@gmail.com>
2023-04-08 10:33:59 +0530

    done with login functionality

--- php/loginsubmission.php
+++ php/loginsubmission.php
@@ -1,7 +1,29 @@
 <?php
-    $data['email'] = $_POST['email'];
-    $data['password'] = $_POST['password'];
-    echo json_encode($data);
-    exit;
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // connection
+    $conn = mysqli_connect("localhost", "root", "", "auth");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM `userdetails` WHERE `email` = '$email'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            if($row['password'] == $password){
+                echo("login is sucessfull");
+            }else{
+                echo('incorrect password');
+            }
+        }
+      } else {
+        echo "no user exists";
+      }
+
+
+    $conn->close();
 
 ?>
